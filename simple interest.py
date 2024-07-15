842f51231d034d5b63b13aac4d114606f2e674a2
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-15 18:15:07 +0530

    Update simple interest.py

--- simple interest.py
+++ simple interest.py
@@ -8,3 +8,6 @@ rate = 5
 
 result = calculate_simple_interest(principal, time, rate)
 print("Simple Interest:", result)
+
+output :-
+![Screenshot 2024-07-15 181029](https://github.com/user-attachments/assets/2c83e665-e4e8-42c8-81ed-616f571f05b7)
