a325e2e9e05aaf4302954df9ab8ac593ac97fbcb
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-15 18:33:54 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -3,3 +3,6 @@ for i in range(1, 11):
 
 for i in range(10, 0, -1):
     print(i)
+
+output :-
+![Screenshot 2024-07-15 183120](https://github.com/user-attachments/assets/5270d067-fba3-4584-8c27-4a2af154083e)
