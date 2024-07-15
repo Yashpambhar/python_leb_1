ea7f8de8929603bca986b1c1e565a29fca7c97ef
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-15 18:02:53 +0530

    Calculate.py

--- Calculate.py
+++ Calculate.py
@@ -10,3 +10,8 @@ print(f"Addition: {addition}")
 print(f"Subtraction: {subtraction}")
 print(f"Multiplication: {multiplication}")
 print(f"Division: {division}")
+
+
+output:-
+
+![Screenshot 2024-07-15 175941](https://github.com/user-attachments/assets/83e200ea-e33e-4868-8511-b6940670fe08)
